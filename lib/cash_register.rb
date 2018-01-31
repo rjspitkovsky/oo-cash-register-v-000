0c858728380caa0a29fecb75b845525f42bfbd5a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-31 21:10:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += price*quantity
-    @@items << title
+    @@items << self 
   end
 
   def apply_discount
@@ -24,7 +24,7 @@ class CashRegister
 
   def items
     @@items
-  end 
+  end
 
 
 
