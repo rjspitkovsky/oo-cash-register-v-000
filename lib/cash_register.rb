dcf0c3be945fe98f937d499a08a6d1cad7387c1f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-31 21:31:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,11 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price*quantity
     @last_transaction = price*quantity
-    @items << title*quantity 
+    if quantity == 1 
+      @items << title 
+    elsif quantity > 1 
+      @items << title*quantity.split 
+    end 
   end
 
   def apply_discount
